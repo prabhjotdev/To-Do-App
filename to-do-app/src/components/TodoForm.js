960ee81986cf5757e0494e0a3dcd3bd5ca09a01d
PJ Dev <prabhjotdev@live.com>
2022-05-11 21:15:29 -0400

    Feature Created
    Created all features with CSS.

---
+++ to-do-app/src/components/TodoForm.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect, useRef } from "react";
+
+function TodoForm(props) {
+  const [input, setInput] = useState(props.edit ? props.edit.value : '');
+  
+  const inputRef = useRef(null);
+
+  useEffect(()=>{
+      inputRef.current.focus();
+  })
+
+  const handleInputChange = (e) =>{
+    setInput(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.onSubmit({
+      id: Math.floor(Math.random() * 1000000),
+      text: input
+    });
+    setInput('');
+  };
+
+
+  return (
+    <form className="todo-form" onSubmit={handleSubmit}>
+      {props.edit ? (
+      <><input
+        className="todo-input"
+        type="text"
+        placeholder="Update todo"
+        name="text"
+        value={input}
+        onChange={handleInputChange}
+        ref={inputRef}
+      />
+      <button className="todo-button">Update Todo</button>
+      </>
+      ) : 
+      (
+      <><input
+        className="todo-input"
+        type="text"
+        placeholder="Add a todo"
+        name="text"
+        value={input}
+        onChange={handleInputChange}
+        ref={inputRef}
+      />
+      <button className="todo-button">Add Todo</button>
+      </>
+      )}
+    </form>
+    
+  );
+}
+
+export default TodoForm;
